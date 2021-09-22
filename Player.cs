652ef4c950acf808d3790d7964f78707c8041410
Pepijn Bakker <bakker.pepijn@gmail.com>
2021-09-22 13:42:09 +0200

    afwefa

---
+++ Player.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace PongAttempt2
+{
+    public class Player
+    {
+        public Texture2D texture;
+        public Rectangle bounds {g}
+        public Vector2 position;
+        
+    }
+}
